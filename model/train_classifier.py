1d89b8fd206788ad21ab491f421074d34819b753
Mohamed-Elbohy1 <91667532+Mohamed-Elbohy1@users.noreply.github.com>
2021-12-19 17:31:53 +0200

    Update train_classifier.py

--- model/train_classifier.py
+++ model/train_classifier.py
@@ -1,7 +1,35 @@
+"""
+
+   Disaster Response Pipeline: 
+    Classifier Trainer Project 
+        > python train_classifier.py <path to sqllite  destination db> <path to the pickle file>
+    
+    Sample Script Execution:
+        > python train_classifier.py ../data/InsertDatabaseName.db classifier.pkl
+    
+    Arguments:
+        1) SQLite destination database path (e.g. dInsertDatabaseName.db) 
+        2) Pickle file name where ML model should be saved (e.g. classifier.pkl)
+"""
+
+
 import sys
 
 
 def load_data(database_filepath):
+       
+    """ 
+    
+    Function to Load Data from a Database
+
+    Arguments:
+        database filepath -> SQLite target database path (for example, InsertDatabaseName.db)
+        X -> a dataframe with features as output
+        Y->  Labels are stored in a dataframe.
+        category names -> is a list of categories 
+
+"""
+    
     engine = create_engine('sqlite:///' + database_filepath)
     df = pd.read_sql_table('DisasterMessages', engine)
     X = df['message']
@@ -11,6 +39,17 @@ def load_data(database_filepath):
 
 
 def tokenize(text):
+    
+    """
+    Tokenize the text function
+    
+    Arguments:
+        text -> A text message that must be tokenized
+    
+    Output:
+        clean_tokens -> A list of tokens retrieved from the supplied text 
+    
+    """
     
     text = re.sub(r"[^a-zA-Z0-9]", ' ', text.lower())
     
@@ -28,6 +67,15 @@ def tokenize(text):
 
 
 def build_model():
+    
+    """
+    
+    Build Pipeline is a function that allows you to create a pipeline.
+
+    A Scikit-ML Pipeline that processes text messages and applies a classifier as an output.
+
+    """
+    
     pipeline = Pipeline([
                         ('vect', CountVectorizer(tokenizer=tokenize)),
                         ('tfidf', TfidfTransformer()),
@@ -45,6 +93,17 @@ def build_model():
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    
+    """
+    Displays the model's performance on test data.
+
+    Arguments: 
+        model: trained model 
+        X_test: feature testing 
+        Y_test: target testing  
+        category_names: target labels
+"""
+    
     Y_pred = model.predict(X_test)
         
     for i in range(len(category_names)):
@@ -53,6 +112,16 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
+    
+    """
+    Saves the model to a Python pickle file  
+    
+   Arguments: 
+        model: trained model
+        model_filepath: path to save model
+
+    """
+    
     pickle.dump(model, open(model_filepath, "wb"))
 
 
